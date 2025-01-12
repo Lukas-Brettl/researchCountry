f192b3ea1e946b50d87bd1b14be959e46a0b0788
brettl <lukas.brettl@seznam.cz>
2025-01-12 20:07:01 +0100

    redo app now its researchCountry and add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Map from './assets/images/map.svg';
+import { useState, useEffect } from 'react';
+export default function App(){
 
-function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState("")
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  useEffect(()=>{
+    fetch(`https://restcountries.com/v3.1/name/${search}`).then(
+    res => {
+      if(!res.ok){
+        throw new Error("Neplatný uživatelský vstup")
+      }
+      return res.json()
+      
+    }
+  )
+  .then(
+    data => data.map(item => console.log(item["name"]["common"]))
+  )
+  },[search])
+  return(
+    <div className="flex flex-col h-full w-full justify-center items-center">
+      {/* <h1 className='font-bold text-xl'>Research country</h1>*/}
+      <img  src={Map} alt="" className='h-full absolute ' style={{filter: "blur(2px)"}}/>
+      <h2> {search}</h2>
+      <input onChange={(e) => setSearch(e.target.value)} type="text" className='bg-white border-gray-500 z-10 w-1/5 h-12 border-4 rounded-lg font-semibold outline-none pl-2 shadow-black shadow-2xl'/>
+
+    </div>
   )
 }
-
-export default App
